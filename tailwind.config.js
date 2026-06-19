39e9148a884aba24ed273c71b5049ccbe9779b93
Al-Mu'taz-Billah <mu3tazf@gmail.com>
2026-06-19 13:34:48 +0300

    fix: update tailwind content paths to build styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,html}", // هنا يتم توجيه Tailwind لقراءة كافة ملفات المكونات
   ],
   theme: {
     extend: {
