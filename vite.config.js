1b703bab3c6b5285dc0c104d462b4c904016334b
Al-Mu'taz-Billah <mu3tazf@gmail.com>
2026-06-19 14:24:10 +0300

    force update ropme data

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // هذا السطر يجبر المتصفح على قراءة ملفات الـ جافاسكربت والـ CSS بشكل صحيح
 })
