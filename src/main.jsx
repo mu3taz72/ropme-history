357a18edd83e552ae9c0f3d20afe3d065ade603f
Al-Mu'taz-Billah <mu3tazf@gmail.com>
2026-06-20 19:37:20 +0300

    fix: restore dynamic grouping and force strict entry points map link

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // التأكيد على قراءة الملف القياسي الوحيد المتبقي
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
